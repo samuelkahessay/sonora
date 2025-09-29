524efaff66fbf8807ae723d72afe2c3187a03fa0
Samuel Kahessay <kahessay@icloud.com>
2025-09-29 10:40:00 -0600

    Add x button to dismiss in SettingsView

--- Sonora/Features/Settings/UI/SettingsView.swift
+++ Sonora/Features/Settings/UI/SettingsView.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 struct SettingsView: View {
 
+    @SwiftUI.Environment(\.dismiss) private var dismiss
+
     var body: some View {
         VStack(spacing: 0) {
             ScrollView {
@@ -42,6 +44,16 @@ struct SettingsView: View {
         }
         .navigationTitle("Settings")
         .navigationBarTitleDisplayMode(.large)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "xmark")
+                }
+                .accessibilityLabel("Dismiss")
+            }
+        }
     }
 }
 
